c8c91bb42ad6fcfb19fba26997060f50b2f09403
Joe Cheng <joe@rstudio.com>
2021-02-04 23:10:43 -0800

    Print times

--- main.R
+++ main.R
@@ -1,5 +1,24 @@
 source("_functions.R")
 
-lst <- list(
-  BH = time_install("BH")
-)
+times <- list()
+
+times$"Install BH" <- time_install("BH")
+
+times$"base::write.csv, 1GB" <- local({
+  fct <- 29.1268
+  df <- data.frame(x1 = runif(1e6 * fct), x2 = runif(1e6 * fct))
+  message("Data generated")
+  system.time({
+    write.csv(df, file = "1gb.csv", row.names = FALSE)
+  })
+})
+
+times$"base::read.csv, 1GB" <- local({
+  system.time({
+    read.csv("1gb.csv")
+  })
+})
+
+unlink("1gb.csv")
+
+print(times)
