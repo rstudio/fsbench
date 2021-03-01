9b315e866b486b07a464ba448fe2c63ed047e884
Joe Cheng <joe@rstudio.com>
2021-03-01 12:41:42 -0800

    Add binary for clearing cache on Linux

---
+++ tools/purge.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+int error(int err) {
+	perror("Error");
+	return 1;
+}
+
+int main() {
+	sync();
+	int fd = open("/proc/sys/vm/drop_caches", O_WRONLY);
+	if (-1 == fd) {
+		return error(errno);
+	}
+	if (-1 == write(fd, "3\n", 2)) {
+		return error(errno);
+	}
+}
