3f0dab41de80aeb64f344957e296bca0ea9a36a5
karl@rstudio.com <karl@rstudio.com>
2021-03-29 13:53:23 -0500

    Add FST random read tests

--- main.R
+++ main.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(fst)
 library(R.utils) # needed for fread to read .gz files
 library(vroom)
 
@@ -49,6 +50,61 @@ for (i in 1:4) {
     unlink(target(sprintf("small_%s.csv", j)))
   }
 }
+
+# FST tests ========
+# Generate a random data frame (approximately 1GB of data), save it to disk,
+# then perform random read tests of different lengths on the file
+size_100mb <- 100*1024*1024
+num_rows <- 0.0625 * size_100mb
+size_per_row <- size_100mb / num_rows
+fst_frame <- data.frame(x1 = runif(num_rows), x2 = runif(num_rows))
+write.fst(fst_frame, target("dataset.fst"))
+
+num_read <- 0
+benchmark("FST random reads, 100MB over 10*10MB reads", system.time({
+  rows_to_read <- (10*1024*1024) / size_per_row
+  while (num_read < size_100mb) {
+    from <- runif(1, 0, num_rows - rows_to_read)
+    to <- from + rows_to_read
+    fst_subset <- read.fst(target("dataset.fst"), NULL, from, to)
+    num_read <- num_read + object.size(fst_subset)
+  }
+}))
+
+num_read <- 0
+benchmark("FST random reads, 100MB over 100*1MB reads", system.time({
+  rows_to_read <- (1*1024*1024) / size_per_row
+  while (num_read < size_100mb) {
+    from <- runif(1, 0, num_rows - rows_to_read)
+    to <- from + rows_to_read
+    fst_subset <- read.fst(target("dataset.fst"), NULL, from, to)
+    num_read <- num_read + object.size(fst_subset)
+  }
+}))
+
+num_read <- 0
+benchmark("FST random reads, 100MB over 1000*100KB reads", system.time({
+  rows_to_read <- (100*1024) / size_per_row
+  while (num_read < size_100mb) {
+    from <- runif(1, 0, num_rows - rows_to_read)
+    to <- from + rows_to_read
+    fst_subset <- read.fst(target("dataset.fst"), NULL, from, to)
+    num_read <- num_read + object.size(fst_subset)
+  }
+}))
+
+num_read <- 0
+benchmark("FST random reads, 100MB over 10000*10KB reads", system.time({
+  rows_to_read <- (10*1024) / size_per_row
+  while (num_read < size_100mb) {
+    from <- runif(1, 0, num_rows - rows_to_read)
+    to <- from + rows_to_read
+    fst_subset <- read.fst(target("dataset.fst"), NULL, from, to)
+    num_read <- num_read + object.size(fst_subset)
+  }
+}))
+
+unlink(target("dataset.fst"))
 
 # Read CRAN logs =========
 
