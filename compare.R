89ededf022c45c9e76789e6ae788474a94789e19
karl@rstudio.com <karl@rstudio.com>
2021-04-12 12:11:48 -0500

    Use Joe's rewritten aggregation to use much more succinct and powerful functional paradigms

--- compare.R
+++ compare.R
@@ -1,16 +1,14 @@
 library(ggplot2)
-library(stringr)
+
+# Compare multiple benchmark runs, generating graphs for each unique filesystem
+# Multiple runs for the same filesystem are averaged together
 
 save_plot <- function(plot, filename) {
   ggsave(filename, plot=plot)
   cat("Plot saved to ", filename)
 }
 
-# Compare multiple benchmark runs, generating graphs for each unique filesystem
-# Multiple runs for the same filesystem are averaged together
-
-#args = commandArgs(trailingOnly = TRUE)
-args = c("ssd=*ssd.csv", "efs=*efs.csv")
+args = commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
   msg <- "No arguments specified.
@@ -19,86 +17,52 @@ Example: Rscript compare.R ssd=*ssd.csv efs=*efs.csv"
   stop(msg)
 }
 
-files_to_read <- list()
-for (arg in args) {
-  arg_pieces <- str_split(arg, fixed("="), 2)
-  if (is.na(arg_pieces[[1]][2])) {
-    stop(sprintf("Invalid argument %s specified. Must be of the form <file system name>=<benchmark files glob>", arg))
-  }
-
-  filesystem_name <- arg_pieces[[1]][1]
-  glob <- arg_pieces[[1]][2]
-
-  files <- Sys.glob(glob)
-  if (length(files) == 0) {
-    stop(sprintf("No files found for glob argument %s", glob))
-  }
-
-  files_to_read[[filesystem_name]] = files
+parsed_args <- regmatches(args, regexec("^(.+?)=(.+)$", args))
+bad_args <- which(vapply(parsed_args, length, integer(1)) == 0)
+if (length(bad_args) > 0) {
+  stop(sprintf("Invalid argument '%s' specified. Must be of the form <file system name>=<benchmark files glob>", args[[bad_args[1]]]))
 }
 
-results <- list()
-for (filesystem_name in attributes(files_to_read)[[1]]) {
-  files <- files_to_read[[filesystem_name]]
-
-  # combine each of the runs for this particular filesystem
-  aggregate_data_frame <- data.frame(matrix(ncol = 5))
-  colnames(aggregate_data_frame) <- c("task", "user", "system", "elapsed", "parallelism")
-  for (file in files) {
-    data_frame <- read.csv(file)
-    aggregate_data_frame <- merge(data_frame, aggregate_data_frame, all.x = TRUE, by.x = c("task", "parallelism"), by.y = c("task", "parallelism"))
-  }
-
-  # average each of the elapsed columns together
-  col_names <- c()
-  for (column in colnames(aggregate_data_frame)) {
-    if (startsWith(column, "elapsed")) {
-      col_names <- append(col_names, c(column))
-    }
-  }
-
-  data_frame_subset <- aggregate_data_frame[, col_names]
-  aggregate_data_frame["average"] = rowMeans(data_frame_subset, na.rm = TRUE)
-  results[[filesystem_name]] = aggregate_data_frame
+arg_matrix <- do.call(rbind, parsed_args)
+files_to_read <- setNames(lapply(arg_matrix[,3], Sys.glob), arg_matrix[,2])
+bad_glob <- which(vapply(files_to_read, length, integer(1)) == 0)
+if (length(bad_glob) > 0) {
+  stop(sprintf("No files found for glob argument '%s'", files_to_read[[bad_glob[1]]]))
 }
 
-# combine all filesystem data frames into one final clean data frame
-final_data_frame <- data.frame()
-num_fs <- length(attributes(results)[[1]])
-final_data_frame <- results[[1]]
-final_data_frame <- final_data_frame[,c("task", "parallelism", "average")]
-colnames(final_data_frame)[3] <- sprintf("%s", attributes(results)[[1]][1])
+final_data_frame <- do.call(rbind, mapply(names(files_to_read), files_to_read, FUN = function(fs, files) {
+  # Read each file into a separate data frame, using read.csv(stringsAsFactors=FALSE)
+  data_frames <- lapply(files, read.csv, stringsAsFactors = FALSE)
+  # Combine all data frames into a single data frame
+  data_frame_all <- do.call(rbind, data_frames)
+  # Fix-up NAs for parallelism - set them to 1 as parallel tests never have a parallelization factor of 1
+  # This makes sure that all of the aggregation functions work correctly
+  data_frame_all$parallelism <- sapply(data_frame_all$parallelism, function(val){if (is.na(val)){ val <- 1};val})
+  # This factor() call is necessary to prevent aggregate() from reordering
+  # by task, alphabetically
+  data_frame_all$task <- factor(data_frame_all$task, unique(data_frame_all$task))
+  # Break data frame into groups of rows based on `task` and `parallelism`, then calculate
+  # mean(elapsed), and return the result as a data frame
+  data_frame_mean <- aggregate(elapsed ~ task+parallelism, data_frame_all, mean)
+  # Return the data in the shape that we ultimately want
+  data.frame(
+    filesystem = fs,
+    grouping = data_frame_mean$task,
+    observation = data_frame_mean$elapsed,
+    parallelism = data_frame_mean$parallelism
+  )
+}, SIMPLIFY = FALSE, USE.NAMES = FALSE))
 
-if (num_fs > 1) {
-  for (i in 2:num_fs) {
-    filesystem_name <- attributes(results)[[1]][i]
-
-    df <- results[[filesystem_name]]
-    sub_df <- df[,c("task", "parallelism", "average")]
-    colnames(sub_df)[3] <- sprintf("%s", filesystem_name)
-
-    final_data_frame <- merge(final_data_frame, sub_df, all.x = TRUE, by.x = c("task", "parallelism"), by.y = c("task", "parallelism"))
-  }
-}
-
-observations <- c()
-groupings <- c()
-parallelisms <- c()
-filesystems <- c()
-for (i in 3:ncol(final_data_frame)) {
-  groupings <- append(groupings, final_data_frame[[1]])
-  parallelisms <- append(parallelisms, final_data_frame[[2]])
-  observations <- append(observations, final_data_frame[[i]])
-  filesystems <- append(filesystems, rep(colnames(final_data_frame)[i], nrow(final_data_frame)))
-}
-
-final_data_frame <- data.frame(observation = observations,
-                               grouping = groupings,
-                               filesystem = filesystems,
-                               parallelism = parallelisms)
+# Rewrap group; this results in a list, each element of which is a character
+# vector of length >= 1
+final_data_frame$grouping <- lapply(final_data_frame$grouping, strwrap, width = 30)
+# Join each character vector's elements, using \n
+final_data_frame$grouping <- vapply(final_data_frame$grouping, paste, character(1), collapse = "\n")
+# Again, need to use factor() to prevent ggplot2 from reordering
+final_data_frame$grouping <- factor(final_data_frame$grouping, unique(final_data_frame$grouping))
 
 # break the data frame into two for separate plots - one for synchronous tests and one for parallel
-sync_data_frame <- subset(final_data_frame, parallelism %in% c(NA))
+sync_data_frame <- subset(final_data_frame, parallelism %in% c(1))
 parallel_data_frame <- final_data_frame[!(final_data_frame$observation %in% sync_data_frame$observation),]
 
 sync_plot <- ggplot(data=sync_data_frame, aes(filesystem, observation, fill=filesystem)) +
