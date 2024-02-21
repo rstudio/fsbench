86f93f9abf3774c6ba3083332ac9e7342e332dec
root <root@ip-10-0-33-182.us-east-2.compute.internal>
2024-02-21 22:56:49 +0000

    Add automated deployment

---
+++ deployment/deployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf /opt/fsbench/*
+rm -rf /root/.cache/
+mkdir -p /opt/fsbench/
+cd /opt/fsbench
+export R_VERSION=4.3.2
+curl -O https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb
+sudo gdebi -y r-${R_VERSION}_1_amd64.deb
+R --version
+git clone https://github.com/samcofer/fsbench
+cd fsbench/
+make setup
+make
