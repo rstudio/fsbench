e671e800e562639f586c07fe7009b128601b7c8e
root <root@ip-10-0-33-182.us-east-2.compute.internal>
2024-02-21 22:59:21 +0000

    don't ask for the permissions to install R

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -5,7 +5,7 @@ mkdir -p /opt/fsbench/
 cd /opt/fsbench
 export R_VERSION=4.3.2
 curl -O https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb
-sudo gdebi -y r-${R_VERSION}_1_amd64.deb
+sudo gdebi -n r-${R_VERSION}_1_amd64.deb
 R --version
 git clone https://github.com/samcofer/fsbench
 cd fsbench/
